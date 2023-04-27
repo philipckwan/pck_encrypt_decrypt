7d5296714647f3e2aa996ae7ae65a0544dab84e2
Philip Kwan <philipckwan@gmail.com>
2023-04-27 20:11:02 +0800

    v1.2.2

--- ce-pck_encrypt_decrypt/app/index.js
+++ ce-pck_encrypt_decrypt/app/index.js
@@ -17,33 +17,53 @@ document.getElementById('encrypt').addEventListener('click', () => {
 document.getElementById('decrypt').addEventListener('click', () => {
   document.forms[0].dataset.action = 'decrypt';
 });
+document.getElementById('encrypt_clipboard').addEventListener('click', () => {
+  document.forms[0].dataset.action = 'encrypt_clipboard';
+});
+document.getElementById('decrypt_clipboard').addEventListener('click', () => {
+  document.forms[0].dataset.action = 'decrypt_clipboard';
+});
 
-document.addEventListener('submit', e => {
-  timeLog(`index.js.submit: 1.1;`);
+document.addEventListener('submit', async e => {
+  //timeLog(`index.js.submit: 1.1;`);
   e.preventDefault();
   const inputText = document.getElementById('inputTextArea').value;
   const password = document.getElementById('password').value;
   const operation = e.target.dataset.action;
 
-  timeLog(`index.js.submit: inputText:[${inputText}]; password:[${password}]; operation:[${operation}]`);
+  //timeLog(`index.js.submit: inputText:[${inputText}]; password:[${password}]; operation:[${operation}]`);
 
   let outputText = "";
   let errorOneLine = "";
-  if (operation === "encrypt") {
+  if (operation.startsWith("encrypt")) {
     const encryptRounds = document.getElementById('encryptRounds').value;
-    timeLog(`__encryptRounds:[${encryptRounds}]`);
+    //timeLog(`__encryptRounds:[${encryptRounds}]`);
     [outputText, errorOneLine] = encryptOneLine(inputText, password, encryptRounds);
   } else {
     [outputText, errorOneLine] = decryptOneLine(inputText, password);
   }
   const outputTextArea = document.getElementById('outputTextArea');
 
-  timeLog(`__outputText: [${outputText}]; errorOneLine:[${errorOneLine}];`);
+  //timeLog(`__outputText: [${outputText}]; errorOneLine:[${errorOneLine}];`);
 
   if (errorOneLine != "") {
     outputTextArea.value = `!!!${errorOneLine}!!!`;
   } else {
-    outputTextArea.value = outputText;
+    if (operation.endsWith("clipboard")) {
+      //timeLog(`__about to write to clipboard;`);
+      let outputMsg = "";
+      try {
+        await navigator.clipboard.writeText(outputText);
+        outputMsg = `(results copied to clipboard; length of text:${outputText.length})`;
+      } catch (ex1) {
+        outputMsg = `!!!ERROR - failed to write to clipboard!!!`;
+        timeLog(`ERROR - failed to write to clipboard; ${ex1};`);
+      }
+      //timeLog(`__done writing to clipboard;`);
+      outputTextArea.value = outputMsg;
+    } else {
+      outputTextArea.value = outputText;
+    } 
   }
 
   /*
